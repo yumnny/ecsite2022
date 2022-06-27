fea81fe84cde3fcf217b8530e46bda44b145e1a7
kawaguchi-trident <hideo.kawaguchi@trident.ac.jp>
2022-06-28 05:59:41 +0900

    ディレクトリ構造修正 css,images,js追加

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/css")
+  eleventyConfig.addPassthroughCopy("./src/images")
+  eleventyConfig.addPassthroughCopy("./src/js")
   return {
     dir: {
       input: "src",
     },
-  };
-};
+  }
+}
