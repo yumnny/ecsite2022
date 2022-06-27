ba34dcdf3cbaaacf56667b97bb4c5f73785048a8
trident_webdesign <webdesigntrident@gmail.com>
2022-06-27 18:11:14 +0900

    初期
    初期データをアップロード

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/images");
+  return {
+    dir: {
+      input: "src",
+    },
+  };
+};
